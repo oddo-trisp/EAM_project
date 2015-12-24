5452e2608c409a9d6265e33cd511edb3cfbffd96
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2015-12-24 13:10:56 +0200

    simple php 'hello world'

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <link rel="stylesheet" href="assets/css/index.css"
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <?php echo "hello world" ?>
+    </body>
+</html>
