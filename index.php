944fcf95f24a0f40220a11b5cd762252fdeef832
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2015-12-25 11:21:29 +0200

    index connected to js and css files

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
-        <link rel="stylesheet" href="assets/css/index.css"
+        <link rel="stylesheet" href="assets/css/index.css">
+        <script src="assets/js/index.js"></script>
         <title>EAM_Project</title>
     </head>
     <body>
         <?php echo "hello world" ?>
+        <button onclick="myFunction()">Try it</button>
     </body>
 </html>
