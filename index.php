e7b9147053f4824e15acf2763f07b7e949f82470
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2015-12-25 12:37:09 +0200

    header is included in main file

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="assets/css/index.css">
         <script src="assets/js/index.js"></script>
         <title>EAM_Project</title>
     </head>
     <body>
-        <?php echo "hello world" ?>
-        <button onclick="myFunction()">Try it</button>
+        <?php include 'header.php' ?>
     </body>
 </html>
