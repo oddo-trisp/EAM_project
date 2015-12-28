cc27bbc420a5a766d423fb31b140e406c4b4c8e8
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2015-12-28 22:12:28 +0200

    connects header,mainbody and footer

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -11,10 +11,16 @@
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="assets/css/index.css">
         <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/mainbody.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
         <script src="assets/js/index.js"></script>
         <title>EAM_Project</title>
     </head>
     <body>
-        <?php include 'header.php' ?>
+        <div id="container">
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+            <div id="mainfile"> <?php include 'mainbody.php' ?></div>
+        </div>
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
     </body>
 </html>
