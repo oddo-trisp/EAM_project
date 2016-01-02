99cb6b605e0dbde9330bcfd8039c8203fe7bcaaf
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-02 18:47:33 +0200

    new footer colors and icons

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <center>
         <div class="logos">
             <a href="http://www.uoa.gr" target="_blank"><img src="assets/img/nkualogo.gif"></a>
+            <a href="http://www.uoa.gr" target="_blank"><img src="assets/img/epeaeklogo.gif"></a>
         </div>
         <div class="copy">
             <p> &copy 2015. All rights reserved. </p>
