318e7eb65ffb66c87435117ee60c02bf2ca0b4dc
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-04 12:58:30 +0200

    correct links

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <center>
         <div class="logos">
             <a href="http://www.uoa.gr" target="_blank"><img src="assets/img/nkualogo.gif"></a>
-            <a href="http://www.uoa.gr" target="_blank"><img src="assets/img/epeaeklogo.gif"></a>
+            <a href="http://www.epeaek.gr/epeaek/el/index.html" target="_blank"><img src="assets/img/epeaeklogo.gif"></a>
         </div>
         <div class="copy">
             <p> &copy 2015. All rights reserved. </p>
