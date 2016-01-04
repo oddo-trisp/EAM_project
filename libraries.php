71a4d1cfc55f8bc6f66ff865bd3c399c6bb0f3e9
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-04 14:28:36 +0200

    ordered list for libraries

---
+++ libraries.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="assets/css/libraries.css">
+        <script src="assets/js/index.js"></script>
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <div id="container">
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+            <div class="libs">
+                <h3> Βιβλιοθήκες Πανεπιστημίου Αθηνών </h3>
+                <ol>
+                    <li><a href="libpage.php">Βιβλιοθήκη Αγγλικής Γλώσσας και Φιλολογίας</a></li>
+                    <li><a href="libpage.php">Βιβλιοθήκη Αρχαιολογίας και Ιστορίας της Τέχνης</a></li>
+                    <li><a href="libpage.php">Βιβλιοθήκη Αστροφυσικής και Αστρονομίας</a></li>
+                </ol>
+            </div>
+        </div>
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
+    </body>
+</html>
