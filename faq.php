98e039f5e6fa4ac82b30c4489c058b596db7b512
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-04 11:38:27 +0200

    simple h4 and p for every q/a

---
+++ faq.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="assets/css/faq.css">
+        <script src="assets/js/index.js"></script>
+        <script src="assets/js/faq.js"></script>
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <div id="container">
+
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+
+            <div class="container">
+                <h3>F.A.Q</h3>
+                <div id="faqs">
+                    <h4>Τί είναι ο Κατάλογος ή OPAC;</h4>
+                    <div>
+                    	<p>Είναι ο Ανοιχτός Δημόσιος Κατάλογος (OPAC - Open Public Access Catalog) των 47 Βιβλιοθηκών του Πανεπιστημίου, μέσω του οποίου είναι δυνατή η αναζήτηση εγγραφών βιβλίων, περιοδικών, πρακτικών συνεδρίων κ.ά., τα οποία και βρίσκονται στις Βιβλιοθήκες.</p>
+                    </div>
+                    <h4>Τι είναι ο Ζέφυρος;</h4>
+                    <div>
+                    	<p>Είναι μια πύλη πρόσβασης στους καταλόγους των Ελληνικών Ακαδημαϊκών Βιβλιοθηκών. Δίνει τη δυνατότητα αναζήτησης διαθέσιμων βιβλίων και περιοδικών στους καταλόγους (OPAC) των περισσότερων ελληνικών Ακαδημαϊκών Ιδρυμάτων.</p>
+                    </div>
+                    <h4>Μπορώ να δανειστώ βιβλία από τη Βιβλιοθήκη μου;</h4>
+                    <div>
+                    	<p>Θα πρέπει να επικοινωνήσετε με τη Βιβλιοθήκη του Τμήματός σας, γιατί δεν υπάρχει ενιαία πολιτική δανεισμού.</p>
+                    </div>
+                    <h4>Τι είναι η βιβλιογραφική Βάση Δεδομένων;</h4>
+                    <div>
+                    	<p>Μια βιβλιογραφική Βάση Δεδομένων περιλαμβάνει βιβλιογραφικά στοιχεία και δεδομένα είτε για ποικίλα θέματα είτε για ένα συγκεκριμένο θέμα. Οι πληροφορίες μπορεί να προέρχονται από Βιβλία, Περιοδικά, Πρακτικά Συνεδρίων και άλλες πηγές πληροφόρησης.</p>
+                    </div>
+                    <h4>Τι σημαίνει ο όρος login via ip recognition ή ip authentication;</h4>
+                    <div>
+                    	<p>Οι όροι αυτοί εμφανίζονται κατά τη σύνδεση με ορισμένες βάσεις του Πανεπιστημίου Αθηνών. Η επιλογή αυτή επιτρέπει στο χρήστη να έχει πρόσβαση στις πλήρεις υπηρεσίες της βάσης, όπως είναι για παράδειγμα τα πλήρη κείμενα περιοδικών. Απαραίτητη προϋπόθεση είναι το σύστημα της βάσης την οποία επιθυμεί να χρησιμοποιήσει ο χρήστης να αναγνωρίζει ότι το IP του τερματικού του χρήστη ανήκει στο Πανεπιστήμιο.</p>
+                    </div>
+                    <h4>Πώς μπορώ να έχω πρόσβαση στις Βιβλιογραφικές Βάσεις;</h4>
+                    <div>
+                    	<p>Θα πρέπει να συνδέομαι με dial up σύνδεση από το Αθηνά Net, ενώ όταν χρησιμοποιώ κάποιο από τα τερματικά (computer) εντός του χώρου του Πανεπιστημίου θα πρέπει να έχω σύνδεση μέσω του Αθηνά Net.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
+    </body>
+</html>
