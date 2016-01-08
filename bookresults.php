b75a546a4669efe8546296fafe3766d48ef53edd
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-08 13:34:14 +0200

    connects with bookinfo

---
+++ bookresults.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="assets/css/bookresults.css">
+        <script src="assets/js/index.js"></script>
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <div id="container">
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+            <div class="wrapper">
+                <h3> Αποτελέσματα Αναζήτησης </h3>
+                <div class="row">
+                   <div class="col-md-7 col-lg-7">
+                       <ul class="media-list main-list">
+                         <li class="media">
+                           <a class="pull-left" href="bookinfo.php">
+                             <img class="media-object" src="http://placehold.it/150x90" alt="...">
+                           </a>
+                           <div class="media-body">
+                             <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
+                             <p class="by-author">By Jhon Doe</p>
+                           </div>
+                         </li>
+                         <li class="media">
+                           <a class="pull-left" href="bookinfo.php">
+                             <img class="media-object" src="http://placehold.it/150x90" alt="...">
+                           </a>
+                           <div class="media-body">
+                             <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
+                             <p class="by-author">By Jhon Doe</p>
+                           </div>
+                         </li>
+                         <li class="media">
+                           <a class="pull-left" href="bookinfo.php">
+                             <img class="media-object" src="http://placehold.it/150x90" alt="...">
+                           </a>
+                           <div class="media-body">
+                             <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
+                             <p class="by-author">By Jhon Doe</p>
+                           </div>
+                         </li>
+                       </ul>
+                   </div>
+               </div>
+            </div>
+        </div>
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
+    </body>
+</html>
