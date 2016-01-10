7b3bf8e2eaf70aa9873a163b706e7e9feaf6c6d6
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-10 17:48:47 +0200

    adds social icons

--- footer.php
+++ footer.php
@@ -4,6 +4,14 @@
             <a href="http://www.uoa.gr" target="_blank"><img src="assets/img/nkualogo.gif"></a>
             <a href="http://www.epeaek.gr/epeaek/el/index.html" target="_blank"><img src="assets/img/epeaeklogo.gif"></a>
         </div>
+        <div class="social">
+            <img src="assets/img/social/facebook.png" title="facebook"/>
+            <img src="assets/img/social/twitter.png" title="twitter"/>
+            <img src="assets/img/social/linkedin.png" title="linkedin"/>
+            <img src="assets/img/social/googleplus.png" title="google+"/>
+            <img src="assets/img/social/rss.png" title="rss"/>
+            <img src="assets/img/social/email-blue.png" title="email"/>
+        </div>
         <div class="copy">
             <p> &copy 2015. All rights reserved. </p>
         </div>
