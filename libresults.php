3f9fdcd1db3066192f973debba043c3b8bba0e27
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-13 11:47:17 +0200

    page to preview search results with well-sm bootstrap class

---
+++ libresults.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="assets/css/libresults.css">
+        <script src="assets/js/index.js"></script>
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <div id="container">
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+            <div class="wrapper">
+                <h3> Αποτελέσματα Αναζήτησης </h3>
+                <div class="well well-sm">
+                    <h4> Βιβλιοθήκη Αγγλικής Φιλολογίας </h4>
+                    <p> Τμήμα Νομικής </p>
+                    <p> Σολωμού 189, 29111 </p>
+                    <p> <a class="more" href="libpage.php"> Περισσότερα </a> </p>
+                </div>
+                <div class="well well-sm">
+                    <h4> Βιβλιοθήκη Θεωρητικών Μαθηματικών </h4>
+                    <p> Τμήμα Μαθηματικών </p>
+                    <p> Αραβαντινού 22, 12292 </p>
+                    <p> <a class="more" href="libpage.php"> Περισσότερα </a> </p>
+                </div>
+                <div class="well well-sm">
+                    <h4> Βιβλιοθήκη Πληροφορικής και Τηλεπικοινωνιών </h4>
+                    <p> Τμήμα ΠΛηροφορικής </p>
+                    <p> Μαυρομιχάλη 12, 22257 </p>
+                    <p> <a class="more" href="libpage.php"> Περισσότερα </a> </p>
+                </div>
+            </div>
+        </div>
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
+    </body>
+</html>
