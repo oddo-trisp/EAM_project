15655872c16b8f386acd8ab8e742c9f733734889
Giorgos Apostolopoulos <giorgos.apost.1994@gmail.com>
2016-01-19 10:31:07 +0200

    announcement page

---
+++ announcementpage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis, Nikos Matthioudakis">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <!-- Latest compiled GoogleApis MAPS -->
+        <script src="https://maps.googleapis.com/maps/api/js"></script>
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link rel="stylesheet" href="assets/css/header.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="assets/css/announcementpage.css">
+        <script src="assets/js/index.js"></script>
+        <title>EAM_Project</title>
+    </head>
+    <body>
+        <div id="container">
+            <div id="headerfile"> <?php include 'header.php' ?></div>
+            <div class="ann">
+                <center>
+                    <h3> Ανακοίνωση </h3>
+                    <p class="date"> Τετάρτη, 20 Ιανουαρίου 2016 </p>
+                    <h4> <b>Προβλήματα στη λειτουργία του Συστήματος</b></h4>
+                    <p>
+                        Σας ενημερώνουμε ότι καθώς συνεχίζονται οι διαδικασίες μετάπτωσης του υπάρχοντος συστήματος αυτοματοποίησης βιβλιοθήκης του ΕΚΠΑ στο νέο συνεργατικό μοντέλο του ILSaS (δράση που εντάσσεται στο έργο του Συνδέσμου Ελληνικών Ακαδημαϊκών Βιβλιοθηκών (ΣΕΑΒ / HEAL-Link) στο ΕΣΠΑ), θα επηρεαστούν οι διαδικασίες δανεισμού και εξυπηρέτησης χρηστών.
+                        Συγκεκριμένα, από την Πέμπτη 5 Νοεμβρίου 2015 ξεκινούν οι διαδικασίες δανεισμού με την σταδιακή ενσωμάτωση των βιβλιοθηκών μας, προκειμένου για να  αντιμετωπιστούν πιθανά προβλήματα στην ομαλή ένταξη της Βιβλιοθήκης του ΕΚΠΑ στο νέο σύστημα.
+                    </p>
+                    <p class="pdf_view"> Δείτε την ανακοίνωση σε pdf: <img class="pdf_download_icon" src="assets/img/pdf_icon.png"/> </p>
+                </center>
+            </div>
+        </div>
+        <div id="footerfile"> <?php include 'footer.php' ?></div>
+    </body>
+</html>
